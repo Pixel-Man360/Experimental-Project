0621bcc821fa1bb3a30558a1968dcc359827e843
Muntasir Chowdhury Mridul <54744758+Mrichigo-crypto@users.noreply.github.com>
2020-09-24 03:06:45 +0600

    Pause Menu added with restart bug

---
+++ Experiment one/Assets/Flappy Bird Scripts/PauseMenuFB.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuFB : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseMenu;
+
+
+    public void Pause()
+    {
+       _pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+      _pauseMenu.SetActive(false);
+       Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+       SceneManager.LoadScene("Flappy Bird");
+   }
+}
